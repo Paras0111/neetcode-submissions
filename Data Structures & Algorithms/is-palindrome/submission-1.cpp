d1c9b6b4f2d0f13c093b7dfbbc12cae20bda7fb1
Paras <92735871+Paras0111@users.noreply.github.com>
2026-07-14 16:46:19 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,38 @@
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+   
+    int len = s.length();
+    int mid = (s.length())/2;
+    int right =  len - 1;
+    int left =  0;
+
+    while (left < right)
+    {
+        if (!isalnum(s[left])) { 
+            left++; 
+            continue;}
+        if (!isalnum(s[right])) {
+            right--;
+            continue;
+            }
+        if(tolower(s[left]) != tolower(s[right]))
+        {
+            return false;
+        }
+        left++; 
+        right--;
+        
+    }
+    return true; 
+    
+
+    }
+};
+/***
+Here the idea is take the centre or the egdes and in a single loop traverse
+the string, till they cross each other. here you could do a lot of things, compare add place etc, 
+
+***/
+
